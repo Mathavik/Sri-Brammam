30676121a2758cc721afe21333017127d2173da3
Mathavik <mathavikanagaraj1@gmail.com>
2026-05-04 11:49:24 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-purple-600">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
