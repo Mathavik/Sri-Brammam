934480c81aca18c296e2b54b1f7eb87105bb047f
jeya-jesina <jeyajesina@gmail.com>
2026-05-04 12:33:52 +0530

    changes some

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import FeaturedCategories from "./pages/homePage/FeaturedCategories";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+
+      {/* Header */}
+      <Header />
+
+      {/* Page Routes */}
       <Routes>
-        <Route path="/" element={<FeaturedCategories/>} />
+        <Route path="/" element={<FeaturedCategories />} />
       </Routes>
+
+      {/* Footer */}
+      <Footer />
+
     </BrowserRouter>
   );
-};
+}
 
 export default App;
